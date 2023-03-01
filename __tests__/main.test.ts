9ab1170b578047253f91a948bdaae06d9a36e478
祁恩泽 <sepveneto@qq.com>
2023-03-01 11:21:11 +0800

    feat: sync npmmirror

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,29 +1,17 @@
-import {wait} from '../src/wait'
+import * as core from '@actions/core'
 import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-import {expect, test} from '@jest/globals'
+import * as main from '../src/main'
+import {expect, jest, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+test('with no name', async () => {
+  const warningSpy = jest.spyOn(core, 'warning')
+  main.run()
+  expect(warningSpy).toHaveBeenCalledWith('without name, change to use the name from package.json')
 })
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+test('set name', () => {
+  process.env['INPUT_NAME'] = 'foo'
+  const debugSpy = jest.spyOn(core, 'debug')
+  main.run()
+  expect(debugSpy).toHaveBeenCalledWith('package name: foo')
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,41 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as path from 'path'
+import {exec} from 'child_process'
+import {readFileSync} from 'fs'
 
-async function run(): Promise<void> {
+function genSyncUrl(name: string): string {
+  return `https://registry-direct.npmmirror.com/${name}/sync?sync_upstream=true`
+}
+
+export async function sync(name: string): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const url = genSyncUrl(name)
+    core.debug(`sync url: ${url}`)
+    exec(`curl -X PUT ${url}`, (err, stdout, stderr) => {
+      if (err) {
+        core.error(err)
+        reject(new Error('sync failed'))
+      }
+      core.info(stdout)
+      core.error(stderr)
+      resolve(true)
+    })
+  })
+}
+
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    let name = core.getInput('name')
+    if (!name) {
+      core.warning(`without name, change to use the name from package.json`)
+      const src = path.resolve(process.cwd(), 'package.json')
+      core.debug(`project root: ${src}`)
+      const packageJson = JSON.parse(readFileSync(src).toString())
+      name = packageJson['name']
+    }
+    core.debug(`package name: ${name}`)
+    await sync(name)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
